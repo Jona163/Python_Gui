77ef95dc6f39d17a0feb4ce334201d16ac027d83
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-25 22:36:48 -0600

    Update main.py

--- login/main.py
+++ login/main.py
@@ -2,3 +2,21 @@
 # Fecha: 25 Septiembre 2024
 # Descripción: Código Python_GUI.
 # GitHub: https://github.com/Jona163
+
+from customtkinter import CTk, CTkFrame, CTkEntry, CTkLabel, CTkButton, CTkCheckBox
+from tkinter import PhotoImage
+
+root = CTk() 
+root.geometry("500x600+350+20")
+root.minsize(480,500)
+root.config(bg ='#010101')
+root.title("Iniciar Sesion")
+
+frame = CTkFrame(root, fg_color='#010101')
+frame.grid(column=0, row = 0, sticky='nsew',padx=50, pady =50)
+
+frame.columnconfigure([0,1], weight=1)
+frame.rowconfigure([0,1,2,3,4,5], weight=1)
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
